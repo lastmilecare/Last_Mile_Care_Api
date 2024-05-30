6b71c718684583454ce31324d527adeb0075f94f
lastmilecare <gaurav@lastmilecare.in>
2024-05-31 00:08:18 +0530

    update code

--- src/middleware/singleFileUpload.js
+++ src/middleware/singleFileUpload.js
@@ -12,12 +12,13 @@ const signatureUpload = async (req, res, next) => {
     if (!req.body.file_url) {
         return res.status(400).send('file_url data   is missing.');
     }
-    const maxBase64Size = 10 * 1024 * 1024; // 2 MB for base64 encoded size
+    const maxBase64Size = 8 * 1024 * 1024; // 2 MB for base64 encoded size
 
     const fileData = req.body.file_url;
     const fileName = req.body.name;
+    console.log(fileData.length);
     if (fileData.length > maxBase64Size) {
-        return res.status(400).send('Base64 string size exceeds 2 MB');
+        return res.status(400).send('Base64 string size exceeds 8 MB');
     }
 
     try {
